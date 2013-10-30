cb24b08d0341dba59550b3642dfa8d6161e8c9e6
Kristian Glass <git@doismellburning.co.uk>
2013-10-30 15:42:00 +0000

    HipChat notifier script

---
+++ README.md
@@ -0,0 +1,1 @@
+Script just using the Python stdlib (and cURL) to send HipChat notifications (lack of dependencies => single file => easy to drop in to other scenarios)
---
+++ hipchat_notifier.py
@@ -0,0 +1,17 @@
+# Script just using the Python stdlib (and cURL) to send HipChat notifications (lack of dependencies => single file => easy to drop in to other scenarios)
+
+import argparse
+import subprocess
+import urllib
+
+parser = argparse.ArgumentParser(description="Send HipChat notifications")
+parser.add_argument('--room-id', required=True, help="")
+parser.add_argument('--from', required=True)
+parser.add_argument('--message', required=True)
+parser.add_argument('--auth-token', required=True)
+
+args = parser.parse_args()
+
+hipchat_data = urllib.urlencode(vars(args))
+
+print subprocess.check_output(["curl", "--silent", "--data", hipchat_data, "https://api.hipchat.com/v1/rooms/message"])
